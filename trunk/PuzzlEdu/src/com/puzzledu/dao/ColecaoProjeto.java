c47053aa76077e8b04534cc6a604861f99a116b7
rubeansite <rubeansite@bc39192f-da72-7b4c-e588-2a8a4fc87394>
2011-08-12 02:09:13 +0000

    remoção de código não mais utilizado.

--- trunk/PuzzlEdu/src/com/puzzledu/dao/ColecaoProjeto.java
+++ trunk/PuzzlEdu/src/com/puzzledu/dao/ColecaoProjeto.java
@@ -3,10 +3,6 @@ package com.puzzledu.dao;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.puzzledu.basica.Classe;
-import com.puzzledu.basica.Metodo;
-import com.puzzledu.basica.Parametro;
-import com.puzzledu.basica.Variavel;
 import com.puzzledu.gerenciador.Projeto;
 
 /**
@@ -17,27 +13,12 @@ public class ColecaoProjeto {
 
 	public ColecaoProjeto() {
 		projetos = new ArrayList<Projeto>();
-		addProjetoExemplo();
 	}
 	
 	public void addProjeto(Projeto p){
 		projetos.add(p);
 	}
-	
-	public void addProjetoExemplo() {
-		Projeto projeto = new Projeto(1L, "Projeto Exemplo");
 
-		Classe hello = new Classe();
-		hello.setNome("Hello");
-		hello.setAbstrata(false);
-		hello.addVariavel(new Variavel("imagem", "Image", "/galery/alomundo.png"));	
-		Metodo showMessage = new Metodo("showMessage");
-		showMessage.setRetorno("void");
-		showMessage.adicionarParametro(new Parametro("message", "String"));
-		hello.addMetodo(showMessage);
-		projeto.getRepositorioDados().getColecaoClasse().addClasseFilha(hello, "Object");
-		projetos.add(projeto);
-	}
 
 	public Projeto buscarPorNome(String nome) {
 		Projeto projeto = null;
